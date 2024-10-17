76e3db9f6c1a5c114f4ba581a8bcd62f216042a4
Michael Goldstein <michael.goldstein@digitalreasoning.com>
2024-10-16 21:15:16 -0500

    initial commit

---
+++ filesync.py
@@ -0,0 +1,64 @@
+import photosdl
+import sys, getopt
+from os import listdir, remove
+from os.path import isfile, join, exists
+
+
+def main(argv):
+    photo_dir = "./"
+    url = ''
+    port = ''
+    username = ''
+    password = ''
+
+    try:
+        opts, args = getopt.getopt(argv,"hu:p:U:P:d:",["username=","password=","url=","port=","directory="])
+    except getopt.GetoptError:
+        print ('filesync.py -u <username> -p <password> -U <url> -P <port> -d <dir>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('filesync.py -u <username> -p <password> -U <url> -P <port> -d <dir>')
+            sys.exit()
+        elif opt in ("-u", "--username"):
+            username = arg
+        elif opt in ("-p", "--password"):
+            password = arg
+        elif opt in ("-U", "--url"):
+            url = arg
+        elif opt in ("-P", "--port"):
+            port = arg
+        elif opt in ("-d", "--directory"):
+            photo_dir = arg
+
+
+    current_files = [f for f in listdir(photo_dir) if isfile(join(photo_dir, f))]
+
+    phdl = photosdl.Photos(url, port, username, password, secure=True, cert_verify=True, dsm_version=7, debug=True, otp_code=None)
+    additional= ["thumbnail","resolution","orientation","video_convert","video_meta","address"]
+    items = phdl.get_album_items('kitchen-dash', additional=additional)
+
+    parsed_items = phdl.parse_items(items['data']['list'])
+
+    if len(parsed_items) < 5:
+        print("Only %s pictures, exiting" % len(parsed_items))
+        return
+    
+    for cache_key, unit_id in parsed_items.items():
+        if "%s.jpg" % cache_key in current_files:
+            current_files.remove("%s.jpg" % cache_key)
+        else:
+            dl = phdl.download_item(cache_key=cache_key, unit_id=unit_id)
+            with open("%s%s.jpg" % (photo_dir,cache_key), "wb") as binary_file:
+                binary_file.write(dl.content)
+
+    for current_file in current_files:
+        if current_file[-4:] != ".jpg":
+            continue
+        filename = "%s%s" % (photo_dir,current_file)
+
+        if exists(filename):
+            remove(filename)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ photosdl.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+from typing import Optional
+import json
+from synology_api import photos
+
+class Photos(photos.Photos):
+
+    def __init__(self,
+                 ip_address: str,
+                 port: str,
+                 username: str,
+                 password: str,
+                 secure: bool = False,
+                 cert_verify: bool = False,
+                 dsm_version: int = 7,
+                 debug: bool = True,
+                 otp_code: Optional[str] = None,
+                 ) -> None:
+
+        super(Photos, self).__init__(ip_address, port, username, password, secure, cert_verify,
+                                     dsm_version, debug, otp_code)
+
+    # https://<IP_ADDRESS>/photo/webapi/entry.cgi?api=SYNO.Foto.Search.Search&method=list_item
+    # &version=1&offset=0&limit=10&keyword=%22Iceland%22
+
+    # https://syn.michaelgoldstein.co:5001/webapi/
+
+    #https://syn.michaelgoldstein.co:5001/webapi//photo/webapi/entry.cgi?api=SYNO.Foto.Search.Search
+
+
+    def get_album_items(self, album_name: str, additional: Optional[str | list[str]] = None) -> dict[str, object] | str:
+        if additional is None:
+            additional = []
+        api_name = 'SYNO.Foto.Search.Search'
+        req_param = {'version': '6', 'keyword': json.dumps(album_name), 'offset': '0', 'limit': '500', 
+                     'method': 'list_item',
+                     'additional': json.dumps(additional)}
+        
+        return self.request_data(api_name, 'entry.cgi/SYNO.Foto.Search.Search', req_param, method='post')
+    
+    @staticmethod
+    def parse_items(items: list[object]) -> dict[str,str]:
+        parsed_items = {}
+        for item in items:
+            parsed_items[item['additional']['thumbnail']['cache_key']] = item['additional']['thumbnail']['unit_id']
+        return parsed_items
+
+    def download_item(self, cache_key: str, unit_id: str):
+        api_name = 'SYNO.Foto.Download'
+        req_param = {'version': '2', 'method': 'download', 'download_type': 'optimized_jpeg','cache_key': cache_key, 'unit_id': json.dumps([unit_id])}
+        return self.request_data(api_name, 'entry.cgi', req_param, method='get', response_json=False)
+
